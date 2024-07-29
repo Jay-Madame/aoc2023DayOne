5a1d1e618638148ac79824582bc7068ec482837b
Jay <jay@Jays-MacBook-Air.local>
2024-07-29 15:56:09 -0500

    IP: Currently working on getting right calculated output from input, but does not display any compiler errors

--- DayOne.java
+++ DayOne.java
@@ -5,37 +5,43 @@ import java.io.IOException;
 
 public class DayOne {
     public static void main(String[] args) {
-        Reader reader = new Reader(System.in);
-        String filePath = "dayOne.text";
+        String filePath = "dayOne.txt";
 
         boolean isOutput1Full, isOutput2Full;
-        int output1, output2 = 0;
-        int total;
+        int output1 = 0;
+        int output2 = 0;
+        int total = 0;
         String str;
-        isOutput1Full = false;
-        isOutput2Full = false;
 
-        while ((str = reader.readLine()) != null) {
-            char[] ch = new char[str.length()];
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            while ((str = reader.readLine()) != null) {
+                isOutput1Full = false;
+                isOutput2Full = false;
 
-            for (int i = 0; i < str.length(); i++) {
-                ch[i] = str.charAt(i);
-            }
-            for (char c : ch) {
-                if (Character.isDigit(c) && (!isOutput1Full)) {
-                    output1 = c;
-                    isOutput1Full = true;
-                } else if (Character.isDigit(c) && (isOutput1Full)) {
-                    output2 = c;
-                    isOutput2Full = true;
+                char[] ch = str.toCharArray();
+
+                for (char c : ch) {
+                    if (Character.isDigit(c)) {
+                        int digitValue = c - '0';
+                        if (!isOutput1Full) {
+                            output1 = digitValue;
+                            isOutput1Full = true;
+                        } else if (isOutput1Full && !isOutput2Full) {
+                            output2 = digitValue;
+                            isOutput2Full = true;
+                        }
+                    }
                 }
+                if (!(isOutput2Full)) {
+                    output2 = output1;
+                }
+                total += (output1*10) + output2;
+                output1 = 0;
+                output2 = 0;
             }
-            if (!(isOutput2Full)) {
-                output2 = output1;
-            }
-            total = output1 + output2;
-            output1 = 0;
-            output2 = 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            ;
         }
         System.out.println(total);
     }
