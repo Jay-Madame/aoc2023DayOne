3b4ec9d71324d9df4937f694b492b1dbf1162ddc
Jay <jay@Jays-MacBook-Air.local>
2024-07-29 15:46:26 -0500

    Renamed: Renamed dayOne.text to dayOne.txt

--- DayOne.java
+++ DayOne.java
@@ -5,36 +5,38 @@ import java.io.IOException;
 
 public class DayOne {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Reader reader = new Reader(System.in);
+        String filePath = "dayOne.text";
 
         boolean isOutput1Full, isOutput2Full;
         int output1, output2 = 0;
         int total;
-        String str = new String();
+        String str;
         isOutput1Full = false;
         isOutput2Full = false;
 
-        str = scan.nextLine();
-        char[] ch = new char[str.length()];
+        while ((str = reader.readLine()) != null) {
+            char[] ch = new char[str.length()];
 
-        for (int i = 0; i < str.length(); i++) {
-            ch[i] = str.charAt(i);
-        }
-        for (char c : ch) {
-            if (Character.isDigit(c) && (!isOutput1Full)) {
-                output1 = c;
-                isOutput1Full = true;
-            } else if (Character.isDigit(c) && (isOutput1Full)) {
-                output2 = c;
-                isOutput2Full = true;
+            for (int i = 0; i < str.length(); i++) {
+                ch[i] = str.charAt(i);
             }
+            for (char c : ch) {
+                if (Character.isDigit(c) && (!isOutput1Full)) {
+                    output1 = c;
+                    isOutput1Full = true;
+                } else if (Character.isDigit(c) && (isOutput1Full)) {
+                    output2 = c;
+                    isOutput2Full = true;
+                }
+            }
+            if (!(isOutput2Full)) {
+                output2 = output1;
+            }
+            total = output1 + output2;
+            output1 = 0;
+            output2 = 0;
         }
-        if (!(isOutput2Full)) {
-            output2 = output1;
-        }
-        total = output1 + output2;
-        output1 = 0;
-        output2 = 0;
-
+        System.out.println(total);
     }
 }
